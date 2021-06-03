c2b3764fbc13966fcb91b99929f88a76619e872c
Priyal Babel <60316882+priyal-babel@users.noreply.github.com>
2021-06-04 02:18:35 +0530

    Final commit

--- database/register.py
+++ database/register.py
@@ -5,35 +5,26 @@ con = MySQLdb.connect(host='localhost', user=user,
                       password=password, database="airline_reservation")
 cursor = con.cursor()
 
-# command = 'CREATE DATABASE airline_reservation'
-# cursor.execute(command)
+command = 'CREATE DATABASE if not exists airline_reservation'
+cursor.execute(command)
 
-# command = 'CREATE TABLE registration(firstname VARCHAR(10),lastname VARCHAR(10),phonenumber VARCHAR(15), emailid nvarchar(255), password varchar(20),gender int, age VARCHAR(3))'
-# cursor.execute(command)
+command = 'CREATE TABLE if not exists registration(firstname VARCHAR(10),lastname VARCHAR(10),phonenumber VARCHAR(15), emailid nvarchar(255), password varchar(20),gender int, age VARCHAR(3))'
+cursor.execute(command)
 
-# command = 'CREATE TABLE booking(firstname VARCHAR(10),lastname VARCHAR(10),phonenumber VARCHAR(15), emailid nvarchar(255), address varchar(200),gender int, age VARCHAR(3))'
-# cursor.execute(command)
-
-# command = "ALTER TABLE booking ADD pnr varchar(6);"
-# cursor.execute(command)
-
-# command = "ALTER TABLE booking ADD source varchar(20);"
-# cursor.execute(command)
-
-# command = "ALTER TABLE booking ADD destination varchar(20);"
-# cursor.execute(command)
-
-# command = "ALTER TABLE booking ADD date varchar(15);"
-# cursor.execute(command)
-
-# command = "ALTER TABLE booking ADD time varchar(20);"
-# cursor.execute(command)
-
-# command = "ALTER TABLE booking ADD airline_name varchar(20);"
-# cursor.execute(command)
-
-# command = "ALTER TABLE booking ADD class varchar(20);"
-# cursor.execute(command)
-
-# command = "ALTER TABLE booking ADD seat varchar(3);"
-# cursor.execute(command)
+command = '''CREATE TABLE if not exists booking(
+                        firstname VARCHAR(10),
+                        lastname VARCHAR(10),
+                        phonenumber VARCHAR(15),
+                        emailid nvarchar(255),
+                        address varchar(200),
+                        gender int,
+                        age VARCHAR(3),
+                        pnr varchar(6),
+                        source varchar(20),
+                        destination varchar(20),
+                        date varchar(15),
+                        time varchar(20),
+                        airline_name varchar(20),
+                        class varchar(20),
+                        seat varchar(3))'''
+cursor.execute(command)
